776fc7c0302a11fd960fe7a49526fb728b6e5e11
Adarsh Gupta <adarshgupta.1312@gmail.com>
2026-02-05 17:39:44 +0530

    Update URL

--- Frontend/Client/src/App.jsx
+++ Frontend/Client/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
     images.forEach((img) => formData.append("images", img));
 
     const response = await axios.post(
-      "https://imagetopdf-converter.onrender.com/convert",
+      "https://imagetopdf-converter.onrender.com",
       formData,
       { responseType: "blob" },
     );
